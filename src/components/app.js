aa5bf868af8c5baf48d60ae42179094e4ff6a018
thom budel <thombudel@gmail.com>
2017-04-17 12:51:19 +0200

    Project setup

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+// src/components/App.js
+
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;  
---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+// src/components/common/Header.js
+
+import React, {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/"
+        activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="/cats" activeClassName="active">Cats</Link>
+    </nav>
+  );
+};
+
+export default Header;  
